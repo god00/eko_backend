d5d625bab9cea0d34c64638700068beaf9bb138b
Phosawat <phosawat.o@gmail.com>
2020-01-29 02:40:14 +0700

    Add frontend build to serve on server

---
+++ build/precache-manifest.57fa0e8fcc6dcec9fe6573d920dfedff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9917ae51478993e6da5dfe75d19173e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2532d1c135fb8711f8e",
+    "url": "/static/css/main.92f82b2f.chunk.css"
+  },
+  {
+    "revision": "dafdc5da94713fbf0144",
+    "url": "/static/js/2.98e44c34.chunk.js"
+  },
+  {
+    "revision": "d2532d1c135fb8711f8e",
+    "url": "/static/js/main.ffbb6cc3.chunk.js"
+  },
+  {
+    "revision": "d8b9c3039e8c71e87f7c",
+    "url": "/static/js/runtime-main.71834f27.js"
+  }
+]);
