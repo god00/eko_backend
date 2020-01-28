8849e321a658919b28aabcfa822754d546bb37f9
Phosawat <phosawat.o@gmail.com>
2020-01-29 00:52:41 +0700

    Update errorMsg

--- controllers/deliver.controller.js
+++ controllers/deliver.controller.js
@@ -9,14 +9,14 @@ const getRoutesById = (id) => {
 
 exports.addNewRoutes = (req, res, next) => {
     try {
-        const { routes, cacheForMinimunCost } = deliverService.extractRoute(JSON.parse(req.body.input));
+        const { routes, cacheForMinimunCost } = deliverService.extractRoute(req.body.input);
         if (routes && cacheForMinimunCost) {
             mapRoutesId[countId] = { routes, cacheForMinimunCost };
             return res.status(200).json({ id: countId++ });
         }
-        return res.status(204).json({ errorMsg: 'Invalid parameter' });
+        return res.status(400).json({ errorMsg: 'Invalid parameter' });
     } catch (err) {
-        return res.status(400).json({ errorMsg: err });
+        return res.status(400).json({ errorMsg: 'Unexpected Error' });
     }
 }
 
@@ -27,22 +27,22 @@ exports.calculateCost = (req, res, next) => {
             const cost = deliverService.calculateCost(req.body.path, routes, cacheForMinimunCost);
             return res.status(200).json({ id: req.body.id, cost });
         }
-        return res.status(204).json({ errorMsg: 'Invalid parameter' });
+        return res.status(400).json({ errorMsg: 'Invalid parameter' });
     } catch (err) {
-        return res.status(400).json({ errorMsg: err });
+        return res.status(400).json({ errorMsg: 'Unexpected Error' });
     }
 }
 
 exports.possibleRoute = (req, res, next) => {
     try {
         const { routes } = getRoutesById(req.body.id);
-        const condition = JSON.parse(req.body.condition);
+        const condition = req.body.condition;
         if (routes && condition) {
             const { possible, paths } = deliverService.possibleRoute(req.body.source, req.body.destination, routes, condition);
             return res.status(200).json({ id: req.body.id, possible, paths });
         }
-        return res.status(204).json({ errorMsg: 'Invalid parameter' });
+        return res.status(400).json({ errorMsg: 'Invalid parameter' });
     } catch (err) {
-        return res.status(400).json({ errorMsg: err });
+        return res.status(400).json({ errorMsg: 'Unexpected Error' });
     }
 }
